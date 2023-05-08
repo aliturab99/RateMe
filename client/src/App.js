e5e32b2bd158c32f974bba8389f0b1f7f2d2b24d
aliturab99 <77963884+aliturab99@users.noreply.github.com>
2023-05-08 17:13:34 +0500

    Alert Component is Ready and working properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import Alert from "./component/library/Alert";
+import { showSuccess } from "./store/actions/alertActions";
 
 function App() {
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      My Rate me App
+      <Button onClick={ () => dispatch(showSuccess("Successfull")) }>Click me</Button>
+      <Alert />
     </div>
   );
 }
