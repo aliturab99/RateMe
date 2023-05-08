a12d98e7396dceb73cf0b7cedcbdc635114bcff8
aliturab99 <77963884+aliturab99@users.noreply.github.com>
2023-05-08 17:32:48 +0500

    Progress Bar Working Properly according to state value of Loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import { Button } from "@mui/material";
 import { useDispatch } from "react-redux";
 import Alert from "./component/library/Alert";
+import ProgressBar from "./component/library/ProgressBar";
 import { showSuccess } from "./store/actions/alertActions";
+import { hideProgressBar, showProgressBar } from "./store/actions/progressBarActions";
 
 function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <Button onClick={ () => dispatch(showSuccess("Successfull")) }>Click me</Button>
+      <Button onClick={() => dispatch(showSuccess("Successfull"))}>Click me</Button>
+      <Button onClick={() => dispatch(showProgressBar())}>Show Progress Bar</Button>
+      <Button onClick={() => dispatch(hideProgressBar())}>Hide Progress Bar</Button>
+      <ProgressBar />
       <Alert />
     </div>
   );
