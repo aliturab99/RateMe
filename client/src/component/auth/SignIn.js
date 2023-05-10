e47cf305aca6804b0927c2634a3561848fef5aed
aliturab99 <77963884+aliturab99@users.noreply.github.com>
2023-05-10 16:53:45 +0500

    Form is completed

--- client/src/component/auth/SignIn.js
+++ client/src/component/auth/SignIn.js
@@ -1,6 +1,39 @@
+import { Box, Button } from "@mui/material"
+import { Field, Form } from "react-final-form"
+import TextInput from "../library/form/TextInput"
+
 function SignIn() {
   return (
-    <div>SignIn</div>
+    <Box p={4} bgcolor="#fff">
+      <Form
+      onSubmit={ data => {
+        console.log("Submitted data",data)
+      }}
+      validate={ (data) => {
+        const errors = {}
+
+        if(!data.email)
+          errors.email = "Email is required"
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email))
+          errors.email = "Invalid Email"
+        if(!data.password)
+          errors.password = "Password is required"
+        return errors
+        }}
+      >
+        {
+          (props) => {
+            return (
+              <form onSubmit={props.handleSubmit} >
+                <Field name="email" type="email" component={TextInput} placeholder="Enter Email" />
+                <Field name="password" type="password" component={TextInput} placeholder="Enter Password" />
+                <Button type="submit" variant="outlined">Sign In</Button>
+              </form>
+            )
+          }
+        }
+      </Form>
+    </Box>
   )
 }
 
---
+++ client/src/component/library/form/TextInput.js
@@ -0,0 +1,19 @@
+import { Box, FormHelperText, TextField } from '@mui/material'
+import React from 'react'
+
+function TextInput(props) {
+    const { input, meta, ...rest } = props;
+    const { touched, error } = meta;
+  return (
+    <Box>
+        <TextField size='small' {...input} {...rest} error={touched && error ? true : false} />
+        <FormHelperText error>
+            {
+                touched && error ? error : <span>&nbsp;</span>
+            }
+        </FormHelperText>
+    </Box>
+  )
+}
+
+export default TextInput
