0a35158bf7985bfe5acd47c4758c90cee12c9165
aliturab99 <syedyawaraliturab@gmail.com>
2023-06-10 16:10:38 +0500

    cLIENT DAY 17

--- client/src/component/user/Users.js
+++ client/src/component/user/Users.js
@@ -7,9 +7,9 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 import EditIcon from '@mui/icons-material/Edit';
 import { loadUsers } from '../../store/actions/userActions';
 import DeleteUser from './DeleteUser';
+import { userTypes } from '../../utils/constants';
 
-function Users({ users, loadUsers }) {
-
+function Users({ users, loadUsers, logedInUserType }) {
   useEffect(() => {
     if (users.length === 0)
       loadUsers()
@@ -32,7 +32,10 @@ function Users({ users, loadUsers }) {
             <TableCell>Name</TableCell>
             <TableCell>Phone</TableCell>
             <TableCell>Email</TableCell>
-            <TableCell>Type</TableCell>
+            {
+              logedInUserType === userTypes.USER_TYPE_SUPER &&
+                <TableCell>Type</TableCell>
+            }
             <TableCell>Actions</TableCell>
           </TableRow>
         </TableHead>
@@ -44,13 +47,16 @@ function Users({ users, loadUsers }) {
                 <TableCell>{user.name}</TableCell>
                 <TableCell>{user.phoneNumber}</TableCell>
                 <TableCell>{user.email}</TableCell>
-                <TableCell>
                 {
-                      user.type == 1 ?
-                        <Chip size='small' label="Super Admin" color="secondary" /> :
-                        <Chip size='small' label="Standard" color="primary" />
+                  logedInUserType === userTypes.USER_TYPE_SUPER &&
+                    <TableCell>
+                    {
+                          user.type == 1 ?
+                            <Chip size='small' label="Super Admin" color="secondary" /> :
+                            <Chip size='small' label="Standard" color="primary" />
+                    }
+                    </TableCell>
                 }
-                </TableCell>
                 <TableCell>
                   <IconButton component={Link} to={`/admin/users/edit/${user._id}`}> <EditIcon /> </IconButton>  
                   <DeleteUser userId={user._id} name={user.name} />  
@@ -63,9 +69,10 @@ function Users({ users, loadUsers }) {
     </Box>
   )
 }
-const mapStateToProps = ({ users }) => {
+const mapStateToProps = ({ users, auth }) => {
   return {
-    users: users.records
+    users: users.records,
+    logedInUserType: auth.user.type
   }
 }
 
