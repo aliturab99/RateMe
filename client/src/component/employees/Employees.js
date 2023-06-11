9f76f46289366d045e8f31584ea18822cc168560
aliturab99 <syedyawaraliturab@gmail.com>
2023-06-11 16:09:43 +0500

    Client Day 18

--- client/src/component/employees/Employees.js
+++ client/src/component/employees/Employees.js
@@ -1,7 +1,7 @@
 import { Avatar, Box, Button, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { loadDepartments } from '../../store/actions/departmentActions'
 import AddIcon from '@mui/icons-material/Add';
 import RefreshIcon from '@mui/icons-material/Refresh';
@@ -9,6 +9,8 @@ import EditIcon from '@mui/icons-material/Edit';
 
 
 function Employees({ departments, loadDepartments }) {
+
+  const { deptId } = useParams();
 
   useEffect(() => {
     // if (departments.length === 0)
@@ -23,7 +25,8 @@ function Employees({ departments, loadDepartments }) {
           Employees
         </Typography>
         <Box>
-          <Button component={Link} to="/admin/employees/add" variant='outlined' startIcon={<AddIcon />} > Add</Button>
+          <Button component={Link} to={`/admin/departments/edit/${deptId}`} variant='outlined' sx={{ mr: 1 }} startIcon={<EditIcon />} > Edit Department</Button>
+          <Button component={Link} to="/admin/employees/add" variant='outlined' startIcon={<AddIcon />} > Add Employees</Button>
         </Box>
       </Box>
 
